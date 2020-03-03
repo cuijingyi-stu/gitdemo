3ba20c8289f3903188dff0038bd22c614f47343a
cuijingyi-stu <1123609041@qq.com>
2020-03-03 11:30:37 +0800

    二次修改

--- cards_tools.py
+++ cards_tools.py
@@ -17,3 +17,23 @@ def show_menu():
     print("")
     print("0.退出系统")
     print("*" * 50)
+
+
+def new_card():
+    """新增名片"""
+    print("-" * 50)
+    print("新增名片")
+    # 1.提示用户输入名片的详细信息
+    name_str = input("请输入姓名：")
+    phone_str = input("请输入电话：")
+    qq_str = input("请输入QQ：")
+    email_str = input("请输入email：")
+    # 2.使用用户输入的信息建立一个名片字典
+    card_dict = {"name": name_str,
+                 "phone": phone_str,
+                 "qq": qq_str,
+                 "email": email_str}
+    # 3.将名片字典添加到列表中
+    card_list.append(card_dict)
+    # 4.提示用户添加成功
+    print("添加%s的名片成功" % name_str)
